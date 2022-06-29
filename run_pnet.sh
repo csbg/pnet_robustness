10168aea1624ffffdb99df8227d321c2b2cf6dbf
Wolfgang Esser-Skala <wolfgang.esser-skala@plus.ac.at>
2022-06-29 08:39:18 +0200

    changes copied PNET results files

--- run_pnet.sh
+++ run_pnet.sh
@@ -21,11 +21,12 @@ do
   python pnet_prostate_paper/analysis/prepare_data.py
 
   target_dir="$results_dir/${random_seed}_${pipeline_seed}"
-  mkdir -p $target_dir/onsplit_average_reg_10_tanh_large_testing/fs
-  cp pnet_prostate_paper/analysis/extracted/* $target_dir
-  cp pnet_prostate_paper/_logs/p1000/pnet/onsplit_average_reg_10_tanh_large_testing/fs/* $target_dir/onsplit_average_reg_10_tanh_large_testing/fs/
-  for file in all_scores.csv log.log P-net_ALL_params.yml P-net_ALL_testing.csv P-net_ALL_training.csv
-  do
-    cp pnet_prostate_paper/_logs/p1000/pnet/onsplit_average_reg_10_tanh_large_testing/$file $target_dir/onsplit_average_reg_10_tanh_large_testing/
-  done
+  mkdir -p $target_dir
+  cp pnet_prostate_paper/analysis/extracted/node_importance_graph_adjusted.csv $target_dir/node_importance.csv
+  cp pnet_prostate_paper/_logs/p1000/pnet/onsplit_average_reg_10_tanh_large_testing/P-net_ALL_testing.csv $target_dir/predictions_test.csv
+  cp pnet_prostate_paper/_logs/p1000/pnet/onsplit_average_reg_10_tanh_large_testing/P-net_ALL_training.csv $target_dir/predictions_train.csv
+  if [ $seed -eq -1 ]
+  then
+    cp pnet_prostate_paper/_logs/*.csv $target_dir
+  fi
 done

a1829ade6f74a7ca39cec83abc50b669cbf66172
Wolfgang Esser-Skala <30843618+skafdasschaf@users.noreply.github.com>
2022-11-29 15:18:23 +0100

    more plots (doc from 2021-10-31)

--- plot_figure_1.R
+++ plot_figure_1.R
@@ -577,6 +577,58 @@ ggsave_publication("z_importance_vs_reachability_spearman",
                    width = 18, height = 9, type = "png")
 
 
+plot_importance_vs_measure(betweenness, "pearson")
+ggsave_publication("z_importance_vs_betweenness_pearson",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(betweenness, "pearson_scaled")
+ggsave_publication("z_importance_vs_betweenness_pearson_scaled",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(betweenness, "spearman")
+ggsave_publication("z_importance_vs_betweenness_spearman",
+                   width = 18, height = 9, type = "png")
+
+
+plot_importance_vs_measure(degree, "pearson")
+ggsave_publication("z_importance_vs_degree_pearson",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(degree, "pearson_scaled")
+ggsave_publication("z_importance_vs_degree_pearson_scaled",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(degree, "spearman")
+ggsave_publication("z_importance_vs_degree_spearman",
+                   width = 18, height = 9, type = "png")
+
+
+plot_importance_vs_measure(indegree, "pearson")
+ggsave_publication("z_importance_vs_indegree_pearson",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(indegree, "pearson_scaled")
+ggsave_publication("z_importance_vs_indegree_pearson_scaled",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(indegree, "spearman")
+ggsave_publication("z_importance_vs_indegree_spearman",
+                   width = 18, height = 9, type = "png")
+
+
+plot_importance_vs_measure(outdegree, "pearson")
+ggsave_publication("z_importance_vs_outdegree_pearson",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(outdegree, "pearson_scaled")
+ggsave_publication("z_importance_vs_outdegree_pearson_scaled",
+                   width = 18, height = 9, type = "png")
+
+plot_importance_vs_measure(outdegree, "spearman")
+ggsave_publication("z_importance_vs_outdegree_spearman",
+                   width = 18, height = 9, type = "png")
+
+
 
 ## Correlations boxplot ----
 
@@ -636,64 +688,127 @@ plot_correlation_boxplots <- function(cor_data) {
 }
 
 plot_correlation_boxplots(cor_data_pearson)
-ggsave_publication("z_correlations_pearson", width = 8, height = 12)
+ggsave_publication("z_correlations_pearson", type = "png", width = 8, height = 12)
 
 plot_correlation_boxplots(cor_data_pearson_scaled)
-ggsave_publication("z_correlations_pearson_scaled", width = 8, height = 12)
+ggsave_publication("z_correlations_pearson_scaled", type = "png", width = 8, height = 12)
 
 plot_correlation_boxplots(cor_data_spearman)
-ggsave_publication("z_correlations_spearman", width = 8, height = 12)
+ggsave_publication("z_correlations_spearman", type = "png", width = 8, height = 12)
 
 
 
 ## Correlation heatmap ----
 
-p <-
+plot_correlation_heatmap <- function(cor_data) {
   cor_data %>%
-  group_by(experiment) %>%
-  summarise(across(indegree:betweenness, mean, na.rm = TRUE)) %>%
-  column_to_rownames("experiment") %>%
-  as.matrix() %>%
-  t() %>%
-  Heatmap(
-    col = circlize::colorRamp2(
-      seq(min(.), max(.), length.out = 9),
-      color("davos", reverse = TRUE)(9),
-    ),
-    name = "correlation of\nnode importance\nscores vs\nnetwork measures",
-    heatmap_legend_param = list(
-      at = round(c(min(.), max(.)), 2),
+
+    # group_by(experiment) %>%
+    # summarise(across(indegree:betweenness, mean, na.rm = TRUE)) %>%
+
+    # group_by(experiment, layer) %>%
+    # summarise(across(indegree:betweenness, mean, na.rm = TRUE)) %>%
+    # unite(experiment, experiment, layer) %>%
+
+    group_by(experiment, layer) %>%
+    summarise(across(indegree:betweenness, mean, na.rm = TRUE)) %>%
+    pivot_longer(indegree:betweenness, names_to = "measure") %>%
+    unite(experiment, experiment, measure) %>%
+    pivot_wider(names_from = layer) %>%
+
+    column_to_rownames("experiment") %>%
+    as.matrix() %>%
+    t() %>%
+    Heatmap(
+      col = circlize::colorRamp2(
+        seq(min(., na.rm = TRUE), max(., na.rm = TRUE), length.out = 9),
+        color("davos", reverse = TRUE)(9),
+      ),
+      name = "correlation of\nnode importance\nscores vs\nnetwork measures",
+      heatmap_legend_param = list(
+        at = round(c(min(., na.rm = TRUE), max(., na.rm = TRUE)), 2),
+        border = FALSE,
+        grid_width = unit(2, "mm"),
+        labels_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+        legend_height = unit(15, "mm"),
+        title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT)
+      ),
+
+      row_dend_gp = gpar(lwd = 0.5),
+      row_title = "network measure",
+      row_title_side = "right",
+      row_title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+      row_names_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+
+      column_dend_gp = gpar(lwd = 0.5),
+      column_title = "experiment",
+      column_title_side = "bottom",
+      column_title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+      column_names_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+
+      width = unit(5, "mm") * ncol(.),
+      height = unit(5, "mm") * nrow(.),
       border = FALSE,
-      grid_width = unit(2, "mm"),
-      labels_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
-      legend_height = unit(15, "mm"),
-      title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT)
-    ),
+    )
+}
 
-    row_dend_gp = gpar(lwd = 0.5),
-    row_title = "network measure",
-    row_title_side = "right",
-    row_title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
-    row_names_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+plot_correlation_heatmap <- function(cor_data) {
+  cor_data %>%
+    group_by(experiment, layer) %>%
+    summarise(across(indegree:betweenness, mean, na.rm = TRUE)) %>%
+    pivot_longer(indegree:betweenness, names_to = "measure") %>%
+    unite(experiment, experiment, measure) %>%
+    pivot_wider(names_from = layer) %>%
+    column_to_rownames("experiment") %>%
+    as.matrix() %>%
+    t() %>%
+    Heatmap(
+      col = circlize::colorRamp2(
+        seq(min(., na.rm = TRUE), max(., na.rm = TRUE), length.out = 9),
+        color("davos", reverse = TRUE)(9),
+      ),
+      name = "correlation of\nnode importance\nscores vs\nnetwork measures",
+      heatmap_legend_param = list(
+        at = round(c(min(., na.rm = TRUE), max(., na.rm = TRUE)), 2),
+        border = FALSE,
+        grid_width = unit(2, "mm"),
+        labels_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+        legend_height = unit(15, "mm"),
+        title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT)
+      ),
 
-    column_dend_gp = gpar(lwd = 0.5),
-    column_title = "experiment",
-    column_title_side = "bottom",
-    column_title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
-    column_names_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+      cluster_rows = FALSE,
+      cluster_columns = FALSE,
 
-    width = unit(15, "mm"),
-    height = unit(25, "mm"),
-    border = FALSE,
-  )
-ggsave_publication("z_corr_heatmap", plot = p,
+      row_dend_gp = gpar(lwd = 0.5),
+      row_title = "layer",
+      row_title_side = "right",
+      row_title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+      row_names_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+
+      column_dend_gp = gpar(lwd = 0.5),
+      column_title = "experiment_measure",
+      column_title_side = "bottom",
+      column_title_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+      column_names_gp = gpar(fontsize = BASE_TEXT_SIZE_PT),
+
+      width = unit(5, "mm") * ncol(.),
+      height = unit(5, "mm") * nrow(.),
+      border = FALSE,
+    )
+}
+
+(p <- plot_correlation_heatmap(cor_data_pearson))
+ggsave_publication("z_corr_heatmap_pearson3", plot = p,
+                   width = 10, height = 6, type = "png")
+
+(p <- plot_correlation_heatmap(cor_data_pearson_scaled))
+ggsave_publication("z_corr_heatmap_pearson_scaled", plot = p,
                    width = 6, height = 6, type = "png")
 
-
-
-
-
-
+(p <- plot_correlation_heatmap(cor_data_spearman))
+ggsave_publication("z_corr_heatmap_spearman", plot = p,
+                   width = 6, height = 6, type = "png")
 
 
 
